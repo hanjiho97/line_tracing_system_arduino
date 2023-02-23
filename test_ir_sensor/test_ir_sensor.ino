1df2e4805bed70060936fad8c8443307d4af1221
Youngjoon Choi <venbo12@gmail.com>
2023-02-23 09:36:40 +0900

    add test_ir_sensor

--- test_ir_sensor/test_ir_sensor.ino
+++ test_ir_sensor/test_ir_sensor.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+int ir_pin = 7;
 
+void setup() {
+  
+  pinMode(ir_pin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int value = digitalRead(ir_pin);
 
+  if (value == HIGH)
+  {
+    Serial.println("value is high");
+  }
 }
