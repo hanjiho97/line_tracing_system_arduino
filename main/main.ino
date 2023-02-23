073dc2ca34247e837499d60458af9e254c3d0ec9
Youngjoon Choi <venbo12@gmail.com>
2023-02-24 08:31:42 +0900

    merge line_follower

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,24 @@
 #include "decision_maker.h"
 #include "common_params.h"
+#include "narrow_line_follower.h"
 
 DecisionMaker *g_decision_maker_;
-
 SensorData g_sensor_data_;
-Param
-void setup() 
+NarrowLineFollower *g_line_follower_;
+
+void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+
   std::cout << "start" << std::endl;
 
   g_decision_maker_ = new DecisionMaker(STATE_TYPE::INIT);
+  g_line_follower_ = new NarrowLineFollower;
 }
 
-void loop() 
+void loop()
 {
   // Serial.println(".hello")
+  g_line_follower_->follow_line();
 }
